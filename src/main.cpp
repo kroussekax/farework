e56374a048c70e733105223a28b26fd0c0314f24
kroussekax <nvimcpp@proton.me>
2025-03-22 22:25:35 +0700

    Cleaned main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "glm/ext/matrix_clip_space.hpp"
+#include "engine.hpp"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/fwd.hpp"
 #include "stb_image.h"
-#include "texture.hpp"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window, float dt);
@@ -30,74 +29,9 @@ std::map<std::string, glm::vec3> positions;
 glm::mat4 projection;
 
 int main(){
-	projection = glm::perspective(glm::radians(110.0f), 800.0f / 800.0f, 0.1f, 100.0f);
+	Engine engine = Engine("Vyyxka", 800, 800);
+	engine.run();
 
-	Texture mcdirt(GL_TEXTURE_2D, "res/img/cool.png");
-
-	// applying mattrices
-	unsigned int modelLoc = glGetUniformLocation(shader.get_id(), "model");
-
-	unsigned int projectionLoc = glGetUniformLocation(shader.get_id(), "projection");
-	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-	positions = {
-		{"joe", glm::vec3(0.0f, 0.0f, 0.0f)},
-		{"enemy1", glm::vec3(1.0f, -2.0f, 0.0f)},
-		{"enemy2", glm::vec3(-2.0f, -2.0f, 0.0f)},
-	};
-
-	float last_time = 0.0f;
-	float delta = 0.0f;
-
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& guiIO = ImGui::GetIO(); (void)guiIO;
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
-
-	while(!glfwWindowShouldClose(window)){
-		delta = getDeltaTime(last_time);
-
-		// Render
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		// Input
-		if(!guiIO.WantCaptureMouse)
-			process_input(window, delta);
-
-		shader.use();
-		mcdirt.bind();
-		VAO1.bind();
-		for(auto &e : positions){
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, e.second);
-
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		}
-
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
-	VAO1.Delete();
-	VBO1.Delete();
-
-	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
