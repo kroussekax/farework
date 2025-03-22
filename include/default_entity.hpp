cd3e27a51675a307e14b3e6ec243f6a216820fa1
kroussekax <nvimcpp@proton.me>
2025-03-22 22:26:02 +0700

    Added pos variable and what-not to default entity

--- include/default_entity.hpp
+++ include/default_entity.hpp
@@ -2,9 +2,14 @@
 
 #include "texture.hpp"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 class DefaultEntity{
 public:
 	Texture texture;
+	glm::vec3 pos;
 
-	DefaultEntity(const char* img_path);
+	DefaultEntity(glm::vec3 pos, const char* img_path);
+	DefaultEntity();
 };
--- src/default_entity.cpp
+++ src/default_entity.cpp
@@ -1,5 +1,8 @@
 #include "default_entity.hpp"
 
-DefaultEntity::DefaultEntity(const char* img_path){
+DefaultEntity::DefaultEntity(glm::vec3 pos, const char* img_path): pos(pos){
 	texture = Texture(GL_TEXTURE_2D, img_path);
+}
+
+DefaultEntity::DefaultEntity(){
 }
